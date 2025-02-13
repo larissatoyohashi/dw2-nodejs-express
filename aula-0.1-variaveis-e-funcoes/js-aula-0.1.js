0a2edd97d0fb703c9ba26572e1a5e86a3b62dd93
larissatoyohashi <ltoyohashi@gmail.com>
2025-02-13 18:46:44 -0300

    Variáveis e funções no JS

---
+++ aula-0.1-variaveis-e-funcoes/js-aula-0.1.js
@@ -0,0 +1,17 @@
+//Introdução a javascript 
+//CTRL + ; -> comenta o codigo selecionado
+
+//Declaração de variáveis no JS: CONST + LET + VAR
+
+const nome = "Larissa" //Const precisa de um valor atribuido a ele
+let Nome // Let não precisa de valor atribuido a ele
+
+
+
+var cor = "Azul"
+var cor ="Amarelo"
+let cidade ="Registro"
+cidade = "Jacupiranga"
+
+const message ="Hello, world! Iniciando estudos Javascript!"
+console.log(message)
