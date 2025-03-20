0529fbda39e65257572a101946fce59dbe25ed32
larissatoyohashi <ltoyohashi@gmail.com>
2025-03-19 21:04:39 -0300

    larissatoyohashi

---
+++ aula-0.3-classe-data-e-moeda-exercicios/js-lista-0.3.js
@@ -0,0 +1,140 @@
+//EXERCICIO 01 - CLASSES
+
+class Heroi {
+  constructor(nome, vida, velocidade, forca) {
+    this.nome = nome;
+    this.vida = vida;
+    this.velocidade = velocidade;
+    this.forca = forca;
+  }
+
+  correr() {
+    return "O heroi esta correndo!";
+  }
+
+  andar() {
+    return "O heroi esta andando!";
+  }
+
+  atacar() {
+    return "O heroi esta atacando!";
+  }
+
+  defender() {
+    return "O heroi esta defendendo!";
+  }
+}
+
+homemAranha = new Heroi();
+homemAranha.nome = "Homem-Aranha";
+homemAranha.vida = "100%";
+homemAranha.velocidade = "120 km/h";
+homemAranha.forca = "100/150";
+
+homemAranha.teia = 1;
+
+homemAranha.sentidoAranha = function () {
+  return "O Homem-Aranha detectou perigo por perto.";
+};
+
+superMan = new Heroi();
+superMan.nome = "SuperMan";
+superMan.vida = "100%";
+superMan.velocidade = "200 km/h";
+superMan.forca = "150/150";
+
+superMan.podeVoar = 1;
+
+superMan.visaoCalor = function () {
+  return "O Superman esta usando sua visão de calor.";
+};
+
+Batman = new Heroi();
+Batman.nome = "Batman";
+Batman.vida = "100%";
+Batman.velocidade = "20 km/h";
+Batman.forca = "50/150";
+
+Batman.esconder = 1;
+
+Batman.investigar = function () {
+  return "O Batman esta investigando um crime.";
+};
+
+console.log("Listando Homem-Aranha...");
+console.log(
+  `O heroi ${homemAranha.nome}, tem ${homemAranha.vida} de vida, velocidade de ${homemAranha.velocidade} e forca ${homemAranha.forca}.`
+);
+
+console.log(homemAranha.teia);
+console.log(homemAranha.sentidoAranha());
+console.log(homemAranha.correr());
+console.log(homemAranha.andar());
+console.log(homemAranha.atacar());
+console.log(homemAranha.defender());
+
+console.log();
+console.log("Listando Superman...");
+console.log(
+  `O heroi ${superMan.nome}, tem ${superMan.vida} de vida, velocidade de ${superMan.velocidade} e forca ${superMan.forca}.`
+);
+
+console.log(superMan.podeVoar);
+console.log(superMan.visaoCalor());
+console.log(superMan.correr());
+console.log(superMan.andar());
+console.log(superMan.atacar());
+console.log(superMan.defender());
+
+console.log();
+console.log("Listando Batman...");
+console.log(
+  `O heroi ${Batman.nome}, tem ${Batman.vida} de vida, velocidade de ${Batman.velocidade} e forca ${Batman.forca}.`
+);
+
+console.log(Batman.esconder);
+console.log(Batman.investigar());
+console.log(Batman.correr());
+console.log(Batman.andar());
+console.log(Batman.atacar());
+console.log(Batman.defender());
+
+// EXERCICIO 02 - MANIPULAÇÃO DE DATAS
+
+const dataAtual = new Date();
+
+dataAtual.setDate(dataAtual.getDate() + 3);
+dataAtual.setMonth(dataAtual.getMonth() + 2);
+dataAtual.setYear(dataAtual.getFullYear() + 1);
+
+const exibirData = () => {
+  return `${dataAtual.getDate()}/${dataAtual.getMonth() + 1}/${dataAtual.getFullYear()}`;
+};
+
+console.log(exibirData());
+
+// EXERCICIO 03 - FORMATAÇÃO DE MOEDAS
+
+const salDol = (salReal) => {
+  return salReal * 0.176;
+};
+
+const salEur = (salReal) => {
+  return salReal * 0.16;
+};
+
+console.log(
+  `O salario em dolar eh ${salDol(2000).toLocaleString("en", {
+    style: "currency",
+    currency: "USD",
+  })} `
+);
+
+console.log(
+  `O salario em euro eh ${salEur(2000).toLocaleString("en", {
+    style: "currency",
+    currency: "EUR",
+  })} `
+);
+
+// EXERCICIO 04 - FORMATAÇÃO DE STRINGS
--- aula-0.3-classe-data-e-moeda/js-aula-0.3-classe-data-e-moeda.js
+++ aula-0.3-classe-data-e-moeda/js-aula-0.3-classe-data-e-moeda.js
@@ -1,3 +1,4 @@
+// SEÇÃO 1
 // CLASSES NO JAVASCRIPT
 // Nome de classes devem iniciar com a primeira letra maiúscula
 
@@ -17,29 +18,133 @@ class Carro {
 
 carroPopular = new Carro("Fiat", "Uno", 2012);
 
-console.log(carroPopular.marca)
-console.log(carroPopular.modelo)
-console.log(carroPopular.ano)
+console.log(carroPopular.marca);
+console.log(carroPopular.modelo);
+console.log(carroPopular.ano);
 
-console.log(`O carro ${carroPopular.marca} é do ano ${carroPopular.ano} e faz ${carroPopular.buzinar()}`)
+console.log(
+  `O carro ${carroPopular.marca} é do ano ${
+    carroPopular.ano
+  } e faz ${carroPopular.buzinar()}`
+);
 
 // Criando instância carroEsportivo
 
 const carroEsportivo = new Carro();
-carroEsportivo.marca = "Chevrolet"
-carroEsportivo.modelo = "Camaro"
-carroEsportivo.ano = "2024"
+carroEsportivo.marca = "Chevrolet";
+carroEsportivo.modelo = "Camaro";
+carroEsportivo.ano = "2024";
 
-console.log(`O carro da marca ${carroEsportivo.marca}, do modelo ${carroEsportivo.modelo} é do ano ${carroEsportivo.ano} e faz ${carroEsportivo.buzinar()}`)
+console.log(
+  `O carro da marca ${carroEsportivo.marca}, do modelo ${
+    carroEsportivo.modelo
+  } é do ano ${carroEsportivo.ano} e faz ${carroEsportivo.buzinar()}`
+);
 
 // Adicionando um novo atributo
 
-carroEsportivo.corNeon = "Azul"
+carroEsportivo.corNeon = "Azul";
 
 // Adicionando um novo método
 
-carroEsportivo.turbo = function() {
-    return "Vrummm quando está acelerando!!!"
-}
+carroEsportivo.turbo = function () {
+  return "Vrummm quando está acelerando!!!";
+};
 
-console.log(`O carro da marca ${carroEsportivo.marca}, do modelo ${carroEsportivo.modelo} e da cor ${carroEsportivo.corNeon} é do ano ${carroEsportivo.ano} e faz ${carroEsportivo.turbo()}`)
+console.log(
+  `O carro da marca ${carroEsportivo.marca}, do modelo ${
+    carroEsportivo.modelo
+  } e da cor ${carroEsportivo.corNeon} é do ano ${
+    carroEsportivo.ano
+  } e faz ${carroEsportivo.turbo()}`
+);
+
+// SEÇÃO 2
+// MANIPULANDO DATAS NO JAVASCRIPT
+// Classe Date() --> Classe nativa da linguagem Javascript
+
+const dataAtual = new Date();
+console.log(typeof dataAtual);
+
+// Pegando o dia atual
+const dia = dataAtual.getDate();
+console.log(`Hoje é dia ${dia}.`);
+
+// Pegando o mês atual
+const mes = dataAtual.getMonth() + 1; // Adicionar o numero um para a contagem de meses começar pelo numero 1
+console.log(`Estamos no mes ${mes}.`);
+
+// Pegando o ano atual
+const ano = dataAtual.getFullYear();
+console.log(`Estamos no ano ${ano}`);
+
+// ADICIONANDO DIAS, MESES E ANOS À DATA ATUAL
+
+// Adicionar 10 dias à data atual:
+dataAtual.setDate(dataAtual.getDate() + 1); // setDate faz uma manipulação da data
+
+// Exibe a nova data:
+console.log(`Daqui a 10 dias será dia ${dataAtual.getDate()}.`);
+
+// Adicionar 3 meses à data atual:
+dataAtual.setMonth(dataAtual.getMonth() + 3);
+
+// Exibe o novo mês:
+console.log(`Daqui a três meses será mês ${dataAtual.getMonth() + 1}.`);
+
+// Adicionar 2 anos à data atual:
+dataAtual.setFullYear(dataAtual.getFullYear() + 2);
+
+// Exibe o novo ano
+console.log(`Daqui a dois anos será ${dataAtual.getFullYear()}`);
+
+// SEÇÃO 3
+// TRABALHANDO COM MOEDAS NO JAVASCRIPT
+let salario = 2500.3;
+console.log(salario);
+
+// Mostrando as casas decimais
+console.log(salario.toFixed(2));
+
+// Ocultando as casas decimais
+console.log(salario.toFixed(0));
+
+// Alterando marcador de casa decimal
+console.log(salario.toFixed(2).replace(".", ","));
+
+// Formatação de moedas
+// Mostrando o salário em REAL (R$)
+
+
+// Mostrando o salário em DÓLAR (USD)
+console.log(
+  salario.toLocaleString("en", { style: "currency", currency: "USD" })
+);
+
+// Mostrando o salário em EURO (EUR)
+console.log(
+  salario.toLocaleString("en", { style: "currency", currency: "EUR" })
+);
+
+// CONVERTENDO DE REAL PARA DÓLAR
+const salarioDolar = salario * 0.176;
+console.log(
+  salarioDolar.toLocaleString("en", { style: "currency", currency: "USD" })
+);
+
+// SEÇÃO 4
+// FORMATAÇÃO DE STRING
+const nome = "Larissa Toyohashi";
+
+// Alterando para letras maúisculas
+console.log(nome.toUpperCase());
+
+// Alterando para letras maúisculas
+console.log(nome.toLowerCase());
+
+//Contando caracteres de uma string
+console.log(nome.length);
+
+// Removendo espaços
+const novoNome = nome.replace(/\s/g,"")
+console.log(novoNome.length);
